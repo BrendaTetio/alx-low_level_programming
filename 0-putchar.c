bd54080d7af9bce1cbe56bec1b788b6698bb3acf
BrendaTetio <brendatetio@gmail.com>
2023-06-20 10:43:03 -0700

    file added

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a[] = "stdio";
+		int b;
+
+	for (b = 0; b < 9; b++)
+		_putchar(a[b]);
+	_putchar('\n');
+	return (0);
+}
