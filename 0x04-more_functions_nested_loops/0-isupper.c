cf949d5731c81d3718dbac78a9ef570af796f026
BrendaTetio <brendatetio@gmail.com>
2023-06-22 04:34:07 -0700

    added file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: character to check
+ * return: 0 or 1
+ */
+int_isupper(int)
+
+{
+	if(c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
