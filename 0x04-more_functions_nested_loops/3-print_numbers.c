b98507e71d4965b0686ef99a21059869893d0639
BrendaTetio <brendatetio@gmail.com>
2023-06-22 13:19:13 -0700

    file added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+/*
+ * File: 3-print_numbers.c
+ * Author: BrendaTetio
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9 followed by a new line.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
