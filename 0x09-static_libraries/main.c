4ff1c083728c967b0db1a9b8f570cac5b7be7196
BrendaTetio <brendatetio@gmail.com>
2023-07-07 06:50:21 -0700

    revised

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,85 @@
+// main.c
+#include <stddef.h>
+
+int _putchar(char c) {
+    return 0; // Empty implementation
+}
+
+int _islower(int c) {
+    return 0; // Empty implementation
+}
+
+int _isalpha(int c) {
+    return 0; // Empty implementation
+}
+
+int _abs(int n) {
+    return 0; // Empty implementation
+}
+
+int _isupper(int c) {
+    return 0; // Empty implementation
+}
+
+int _isdigit(int c) {
+    return 0; // Empty implementation
+}
+
+int _strlen(char *s) {
+    return 0; // Empty implementation
+}
+
+void _puts(char *s) {
+    // Empty implementation
+}
+
+char *_strcpy(char *dest, char *src) {
+    return dest; // Empty implementation, returning the destination string
+}
+
+int _atoi(char *s) {
+    return 0; // Empty implementation
+}
+
+char *_strcat(char *dest, char *src) {
+    return dest; // Empty implementation, returning the destination string
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    return dest; // Empty implementation, returning the destination string
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    return dest; // Empty implementation, returning the destination string
+}
+
+int _strcmp(char *s1, char *s2) {
+    return 0; // Empty implementation
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    return s; // Empty implementation, returning the source string
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    return dest; // Empty implementation, returning the destination string
+}
+
+char *_strchr(char *s, char c) {
+    return NULL; // Empty implementation, returning NULL
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    return 0; // Empty implementation
+}
+
+char *_strpbrk(char *s, char *accept) {
+    return NULL; // Empty implementation, returning NULL
+}
+
+char *_strstr(char *haystack, char *needle) {
+    return NULL; // Empty implementation, returning NULL
+}
+
+// Rest of the code, including main() function if necessary
+
