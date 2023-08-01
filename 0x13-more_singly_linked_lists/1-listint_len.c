8ea2850b4048e08524c99742cebacd88de913438
BrendaTetio <brendatetio@gmail.com>
2023-08-01 06:00:31 -0700

    third commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,19 @@
+/*
+ * File: 1-listint_len.c
+ * Author: BrendaTetio
+ */
+
 #include "lists.h"
 #include <stdio.h>
 
 /**
- * print_listint - function that returns the number of elements in a linked listint_t list.
- * @h: pointer to the header of list_t list.
+ * listint_len - Returns the number of elements
+ *               in a linked listint_t list.
+ * @h: A pointer to the head of the listint_t list.
  *
- * Return: number of elements in listint_t.
+ * Return: The number of elements in the listint_t list.
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t nodes = 0;
 
