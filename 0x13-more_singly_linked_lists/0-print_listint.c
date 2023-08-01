2f321a8291fb94e26473fff1dfd02a4ce31e15d8
BrendaTetio <133117934+BrendaTetio@users.noreply.github.com>
2023-08-01 13:50:03 +0300

    updated the file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,18 @@
+/*
+ * File: 0-print_listint.c
+ * Auth: Brenda Tetio
+ */
+
 #include "lists.h"
 #include <stdio.h>
 
 /**
- * print_listint - function that prints all the elements of a listint_t list.
- * @h: pointer to the header of list_t list.
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the list_t list.
  *
- * Return: number of nodes in list_t list.
+ * Return: The number of nodes in the list_t list.
  */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
